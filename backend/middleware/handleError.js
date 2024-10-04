1fcf5b731b36251b2fdcc59d9df3a320af40fe86
abdulhussain01 <abdulhussain.sadriwala@gmail.com>
2024-10-04 23:56:40 +0530

    update errorhandle.js

--- backend/middleware/handleError.js
+++ backend/middleware/handleError.js
@@ -6,37 +6,40 @@ class HandleErrors extends Error {
   }
 }
 
-
 export const errorHandler = (err, req, res, next) => {
-    err.message = err.message || "Internal Server Error";
-    err.statusCode = err.statusCode || 500;
-    if (err.code === 11000) {
-      const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
-      err = new HandleErrors(message, 400);
-    }
-    if (err.name === "JsonWebTokenError") {
-      const message = `Json Web Token is Invalid. Try Again`;
-      err = new HandleErrors(message, 400);
-    }
-    if (err.name === "TokenExpiredError") {
-      const message = `Json Web Token is Expired. Try Login!`;
-      err = new HandleErrors(message, 400);
-    }
-    if (err.name === "CastError") {
-      const message = `Invalid ${err.path}`;
-      err = new HandleErrors(message, 400);
-    }
+  err.message = err.message || "Internal Server Error";
+  err.statusCode = err.statusCode || 500;
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new HandleErrors(message, 400);
+  }
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json Web Token is Invalid. Try Again`;
+    err = new HandleErrors(message, 400);
+  }
+  if (err.name === "TokenExpiredError") {
+    const message = `Json Web Token is Expired. Try Login!`;
+    err = new HandleErrors(message, 400);
+  }
+  if (err.name === "CastError") {
+    const message = `Invalid ${err.path}`;
+    err = new HandleErrors(message, 400);
+  }
 
-    const erroMessage = err.erros
-      ? Object.values(err.errors)
-          .map((error) => error.message)
-          .join(" ")
-      : err.message;
+  const erroMessage = err.erros
+    ? Object.values(err.errors)
+        .map((error) => error.message)
+        .join(" ")
+    : err.message;
 
-    return res.status(err.statusCode).json({
-      success: false,
-      message: erroMessage,
-    });
-  };
+  res.header("Access-Control-Allow-Origin", process.env.MUSICAPP_URL);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  return res.status(err.statusCode).json({
+    success: false,
+    message: erroMessage,
+  });
+};
 
 export default HandleErrors;
