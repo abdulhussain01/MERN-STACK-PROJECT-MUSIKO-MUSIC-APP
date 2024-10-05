f7c1fa0f65a99bb32a42ccf64efec4da91ea78c8
abdulhussain01 <abdulhussain.sadriwala@gmail.com>
2024-10-05 18:05:51 +0530

    update dbconnect.js

--- backend/db/dbConnect.js
+++ backend/db/dbConnect.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 
+const username = encodeURIComponent(process.env.MONGODB_USERNAME);
+const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+
+const mongodbURI = `mongodb+srv://${username}:${password}@musiko-backend.cqffs.mongodb.net/?retryWrites=true&w=majority&appName=musiko-backend`;
+
 const dbConnect = async () => {
   try {
-
-    const connect = await mongoose.connect(process.env.MONGODBURI, {
+    const connect = await mongoose.connect(mongodbURI, {
       dbName: "musiko-backend",
     });
-   
+
     connect.connection.on("connected", () => {
       console.log("db connected");
     });
-
   } catch (error) {
     console.log(error);
   }
