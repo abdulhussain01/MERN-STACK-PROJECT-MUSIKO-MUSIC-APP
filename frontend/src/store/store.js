33eb64452d68a3cabc33951b2ee3b334e1087cfc
abdulhussain01 <abdulhussain.sadriwala@gmail.com>
2024-10-06 13:31:56 +0530

    update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,13 +1,16 @@
-import {configureStore} from "@reduxjs/toolkit"
-import themeReducer from "./slices/theme.slice"
-import playerReducer from "./slices/player.slice"
-import userReducer from "./slices/user.slice"
-const store = configureStore({
-    reducer:{
-        user:userReducer,
-        theme:themeReducer,
-        player:playerReducer,
-    }
-})
+import { configureStore } from "@reduxjs/toolkit";
+import themeReducer from "./slices/theme.slice";
+import playerReducer from "./slices/player.slice";
+import userReducer from "./slices/user.slice";
+const store = configureStore(
+  {
+    reducer: {
+      user: userReducer,
+      theme: themeReducer,
+      player: playerReducer,
+    },
+  },
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-export default store
+export default store;
